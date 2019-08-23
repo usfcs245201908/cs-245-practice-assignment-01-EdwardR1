c38e59fea3758b6e6149b6a1cc2e67815c185084
Edward Rees <erees.hk@gmail.com>
2019-08-23 13:50:51 -0700

    Comments and naming conventions

--- Practice01MathIterative.java
+++ Practice01MathIterative.java
@@ -1,4 +1,9 @@
 public class Practice01MathIterative implements Practice01Math {
+    /**
+     * @param n Input value
+     * @throws Exception if n is less than 0, throws a new exception
+     * @return total value of the factorial of n using iteration
+     */
     public int fact(int n) throws Exception {
         if (n < 0) {
             throw new Exception("n is less than 0!");
@@ -11,19 +16,24 @@ public class Practice01MathIterative implements Practice01Math {
         }
     }
 
+    /**
+     * @param n input value of n
+     * @throws Exception if n is less than 0, throws a new exception
+     * @return fibonacci value at n
+     */
     public int fib(int n) throws Exception {
         if (n < 0) {
             throw new Exception("n is less than 0!");
         } else {
             int f = 0;
-            int tl1 = 0;
-            int tl2 = 1;
+            int tempLast1 = 0;
+            int tempLast2 = 1;
             for (int i = 1; i <= n; i++) {
-                f = tl1 + tl2;
+                f = tempLast1 + tempLast2;
                 if (i % 2 == 0) {
-                    tl1 += tl2;
+                    tempLast1 += tempLast2;
                 } else {
-                    tl2 += tl1;
+                    tempLast2 += tempLast1;
                 }
             }
             return f;
--- Practice01MathRecursive.java
+++ Practice01MathRecursive.java
@@ -1,4 +1,9 @@
 public class Practice01MathRecursive implements Practice01Math {
+    /**
+     * @param n input value of n
+     * @throws Exception throws new exception if n is less than 0
+     * @return return value of the factorial of n using recursion
+     */
     public int fact(int n) throws Exception {
         if (n < 0) {
             throw new Exception("n is less than 0!");
@@ -9,6 +14,11 @@ public class Practice01MathRecursive implements Practice01Math {
         return n * fact(n - 1);
     }
 
+    /**
+     * @param n input value of n 
+     * @throws Exception throws new exception if n is less than 0
+     * @return returns fibonacci value at n using recursion
+     */
     public int fib(int n) throws Exception {
         if (n < 0) {
             throw new Exception("n is less than 0!");
